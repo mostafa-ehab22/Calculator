12a0d0b618f851f79df5b8f91e2eb7539d005fd7
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-06-22 00:03:42 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("display");
+
+function addToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  if (!display.value) return "";
+
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
